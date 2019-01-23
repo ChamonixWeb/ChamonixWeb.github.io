ca18325866addfe1dda776f5427c38649d9b8948
ChamonixWeb <46550712+ChamonixWeb@users.noreply.github.com>
2019-01-24 03:26:54 +0800

    手機板JS修改

--- JS/about.js
+++ JS/about.js
@@ -1,7 +1,7 @@
 
 
 // 滑掉直接關閉聯絡我們視窗
- window.addEventListener('wheel', function (e) {
+window.addEventListener('wheel', function (e) {
     // 滑鼠上滾
     // 往上滑可以直接關閉聯絡我們視窗
     if (e.deltaY < 0) {
@@ -57,4 +57,54 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+// 改善手機介面上滑
+
+
+//滑動起點與終點
+var startY = 0;
+var endY = 0;
+
+
+//監聽window的觸控事件 
+
+//touchstart:手指接觸螢幕的瞬間
+window.addEventListener('touchstart', function (event) {
+
+    var touch = event.targetTouches[0];
+    //紀錄觸控起始點
+    startY = touch.pageY;
+    console.log("起始點:" + startY);
+
+}, false)
+
+//touchmove:手指點下去後移動的時候
+window.addEventListener('touchmove', function (event) {
+
+    var touch = event.targetTouches[0];
+    //紀錄觸控起終點
+    endY = touch.pageY;
+    console.log("滑動距離:" + endY);
+
+}, false)
+
+//touchend:手指離開螢幕的瞬間
+window.addEventListener('touchend', function (event) {
+
+    //終點 - 起點 = 距離   < 10 = 向上滑動如果大於10單位
+    if (endY - startY < 10) {
+
+        console.log("偵測到向上滑動");
+
+        //do something
+        $('#section888').addClass('boxhid');
+    } else if (endY - startY > 10) {
+
+        console.log("偵測到向下滑動");
+
+        //do something
+
+    }
+}, false)
 
